b746511e2a55b2e72d53e172ccdcca7da6da5444
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 03:23:13 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal REAL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge (
+  amount REAL,
+  user_id INTEGER,
+  project_id INTEGER
+);
