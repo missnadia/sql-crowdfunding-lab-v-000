9331d7001d98d2ac76e2702583ed8ff3544035fa
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-17 03:52:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, pledges.amount ORDER BY name;"
+"SELECT projects.title, pledges.amount ORDER BY users.name;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT name, age FROM users INNER JOIN pledges ON users.id = pledges.user_id ORDER BY name;"
+"SELECT name, age FROM users INNER JOIN pledges ON users.id = pledges.user_id ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-""
+"SELECT projects.category"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
